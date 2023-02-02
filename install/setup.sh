95371e0467ae8b36a01542236e222293b1fb145d
Igor Tron <igor_tron@epam.com>
2023-02-02 22:02:33 +0100

    colors refactored

--- install/setup.sh
+++ install/setup.sh
@@ -10,7 +10,7 @@ echo "Containers creating..."
 docker-compose up -d
 echo "Containers created."
 
-echo -e "Composers installation... Yes, there are two composers ${RYellow}:-D"
+echo -e "Composers installation... Yes, there are two composers ${RYELLOW}:-D${COLOR_OFF}"
 docker-compose exec php sh -c "composer install"
 docker-compose exec php sh -c "cd ./cf7-telegram && composer install"
 
@@ -31,14 +31,14 @@ fi
 
 # install WP
 echo "WP database init"
-echo -e -n "${RCyan}Would you init new instance (y), or do nothing (n)? ${RYellow}(y/n)"
+echo -e -n "${ICYAN}Would you init new instance (y), or do nothing (n)? ${RYELLOW}(y/n)${COLOR_OFF}"
 
 read -r item
 case "$item" in
     y|Y)
     echo "WP database init new instance..."
     docker-compose exec php sh -c "wp core install --url=$PROJECT_BASE_URL --title=\"$WP_TITLE\" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email"
-    printf "${RGreen}WP User Admin: ${RYellow}%s \n${RGreen}WP User Pass: ${RYellow}%s\n" $WP_ADMIN $WP_ADMIN_PASS
+    printf "${RGREEN}WP User Admin: ${RYELLOW}%s \n${RGREEN}WP User Pass: ${RYELLOW}%s${COLOR_OFF}\n" $WP_ADMIN $WP_ADMIN_PASS
       ;;
 
     *)
@@ -46,6 +46,6 @@ case "$item" in
       ;;
 esac
 
-echo -e "${RRed}Do not forget update the hosts file with line:"
-echo -e "${RGreen}127.0.0.1 cf7tgdev.loc${Color_Off}"
+echo -e "${RRED}Do not forget update the hosts file with line:"
+echo -e "${RGREEN}127.0.0.1 cf7tgdev.loc${COLOR_OFF}"
 echo "Done."
