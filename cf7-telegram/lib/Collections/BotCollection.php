8e1cdbd35213fee29cd5adbe420fbefa246f7d68
Igor Tron <igor_tron@epam.com>
2023-02-05 17:34:09 +0100

    Collections got refactored

--- cf7-telegram/lib/Collections/BotCollection.php
+++ cf7-telegram/lib/Collections/BotCollection.php
@@ -2,11 +2,7 @@
 
 namespace iTRON\cf7Telegram\Collections;
 
-use Ramsey\Collection\Collection;
-use iTRON\cf7Telegram\wppaCollectionFromConnectionsTrait;
-
 class BotCollection extends Collection {
-	use wppaCollectionFromConnectionsTrait;
 
 	function __construct( array $data = [] ) {
 		$namespace = explode( '\\', __NAMESPACE__ );
