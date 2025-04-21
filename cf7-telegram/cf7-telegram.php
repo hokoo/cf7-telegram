250eddda06f30f52efc37f08ed87239bd7a68f22
Igor Tron <public@itron.pro>
2025-04-21 19:21:29 +0400

    fix init priority

--- cf7-telegram/cf7-telegram.php
+++ cf7-telegram/cf7-telegram.php
@@ -23,7 +23,7 @@ require ( __DIR__ . '/classes/wpcf7telegram.php' );
 
 require __DIR__ . '/vendor/autoload.php';
 
-add_action( 'init', [ Client::getInstance(), 'init' ], 20 );
+add_action( 'init', [ Client::getInstance(), 'init' ], 11 );
 Settings::init();
 
 add_action( 'in_plugin_update_message-' . WPCF7TG_PLUGIN_NAME, 'wpcf7tg_plugin_update_message', 10, 2 );
