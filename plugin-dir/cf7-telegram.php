8320c47baa420ea460b06c499840c7bf4efe9130
Igor Tron <public@itron.pro>
2025-04-24 23:22:17 +0400

    pre-releases checkbox

--- plugin-dir/cf7-telegram.php
+++ plugin-dir/cf7-telegram.php
@@ -73,14 +73,16 @@ add_action(
 	10, 2
 );
 
-$updateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/hokoo/cf7-telegram',
-	WPCF7TG_FILE,
-	'cf7-telegram'
-);
+if ( wpcf7_Telegram::get_instance()->pre_releases ) {
+	$updateChecker = PucFactory::buildUpdateChecker(
+		'https://github.com/hokoo/cf7-telegram',
+		WPCF7TG_FILE,
+		'cf7-telegram'
+	);
 
-if ( defined( 'WPCF7TG_GITHUB_TOKEN' ) ) {
-	$updateChecker->setAuthentication( WPCF7TG_GITHUB_TOKEN );
+	if ( defined( 'WPCF7TG_GITHUB_TOKEN' ) ) {
+		$updateChecker->setAuthentication( WPCF7TG_GITHUB_TOKEN );
+	}
+
+	$updateChecker->setBranch( 'plugin-dist' );
 }
-
-$updateChecker->setBranch( 'pre-releases' );
