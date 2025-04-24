d489ae112a1841033b01242ed4d5c2c45e48c572
Igor Tron <public@itron.pro>
2025-04-24 14:54:38 +0400

    put main file into root

---
+++ cf7-telegram.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Plugin Name: Contact Form 7 + Telegram
+* Description: Sends messages to Telegram-chat
+* Author: Hokku
+* Version: 1.0.1-beta
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: cf7-telegram
+* Domain Path: /languages
+*/
